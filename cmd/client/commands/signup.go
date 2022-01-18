bb7a8d8f43fd9e834e919ce83cd0efdbaafcec57
Sherif <sherif.mowafy@gmail.com>
2022-01-18 19:20:05 +0100

    cli: sign up command

--- cmd/client/commands/signup.go
+++ cmd/client/commands/signup.go
@@ -3,10 +3,14 @@ package commands
 import (
 	"github.com/smowafy/asdf/internal/client"
 	"github.com/spf13/cobra"
+	"fmt"
+	"os"
+	"golang.org/x/term"
+	"syscall"
 )
 
 var SignUpCommand = &cobra.Command{
-	Use:   "signup",
+	Use:   "signup [ACCOUNT_ID]",
 	Short: "sign up with a new account ID",
 	//	Long: `echo long`,
 	Args: cobra.ExactArgs(2),
@@ -14,14 +18,39 @@ var SignUpCommand = &cobra.Command{
 }
 
 func RunSignUpCommand(cmd *cobra.Command, args []string) {
-	accountId, password := args[0], args[1]
-	asdfClient, err := client.NewAsdfClient(password, accountId)
+	accountId := args[0]
+
+	fmt.Printf("password:\n")
+
+	password, err := term.ReadPassword(syscall.Stdin)
 
 	if err != nil {
 		panic(err)
 	}
 
-	if err = asdfClient.SignUp(password, accountId); err != nil {
+	fmt.Printf("password confirmation:\n")
+
+	passwordConfirmation, err := term.ReadPassword(syscall.Stdin)
+
+	if err != nil {
+		panic(err)
+	}
+
+	passwordString := string(password)
+	passwordConfirmationString := string(passwordConfirmation)
+
+	if passwordString != passwordConfirmationString {
+		fmt.Printf("Passwords do not match\n")
+		os.Exit(1)
+	}
+
+	asdfClient, err := client.NewAsdfClient(passwordString, accountId)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err = asdfClient.SignUp(passwordString, accountId); err != nil {
 		panic(err)
 	}
 }
